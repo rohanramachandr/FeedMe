376b4b5bdeff647092b641e35e6545756b177c9d
Rohan Ramachandran <rmramachandr@wisc.edu>
2021-10-29 18:01:03 -0500

    login/register pages

--- app/src/main/java/com/example/feedmewithfirebase/MainActivity.java
+++ app/src/main/java/com/example/feedmewithfirebase/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.feedmewithfirebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClickRegister(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+
 }
