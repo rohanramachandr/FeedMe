472373995d5339061191bc40e699ee44a1204b34
Rohan Ramachandran <rmramachandr@wisc.edu>
2021-11-12 17:21:23 -0600

    added realtime database functionality for registration

--- app/src/main/java/com/example/feedmewithfirebase/RegisterActivity.java
+++ app/src/main/java/com/example/feedmewithfirebase/RegisterActivity.java
@@ -5,17 +5,57 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText regFirstName, regLastName, regUsername, regPassword, regConfirmPassword;
+
+
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        rootNode = FirebaseDatabase.getInstance("https://feedme-55e8d-default-rtdb.firebaseio.com/");
+        reference = rootNode.getReference("Users");
     }
 
-    public void onClickHome(View view) {
-        Intent intent = new Intent(this, HomeActivity.class);
+
+
+
+    public void createAccount(View view) {
+
+        regFirstName = findViewById(R.id.firstName);
+        regLastName = findViewById(R.id.lastName);
+        regUsername = findViewById(R.id.username);
+        regPassword = findViewById(R.id.password);
+        regConfirmPassword = findViewById(R.id.password);
+
+        String firstName = regFirstName.getText().toString();
+        String lastName = regLastName.getText().toString();
+        String username = regUsername.getText().toString();
+        String password = regPassword.getText().toString();
+        String confirmPassword = regConfirmPassword.getText().toString();
+
+
+        UserHelperClass helperClass = new UserHelperClass(firstName, lastName, username, password);
+
+        reference.child(username).setValue(helperClass);
+
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+
+
     }
 }
---
+++ app/src/main/java/com/example/feedmewithfirebase/UserHelperClass.java
@@ -0,0 +1,48 @@
+package com.example.feedmewithfirebase;
+
+public class UserHelperClass {
+    String firstName, lastName, username, password;
+
+    public UserHelperClass(){}
+
+    public UserHelperClass(String firstName, String lastName, String username, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
