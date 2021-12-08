7581b57ad1a1a95fb4b894bd624d0f4f991dcc0e
Colton Raymond <55898834+craymond3@users.noreply.github.com>
2021-12-08 13:24:32 -0600

    Update Seller Recycler View to Match Dan's Buyer Recycler

--- app/src/main/java/com/example/feedmewithfirebase/SellerFragment.java
+++ app/src/main/java/com/example/feedmewithfirebase/SellerFragment.java
@@ -2,23 +2,33 @@ package com.example.feedmewithfirebase;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link SellerFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SellerFragment extends Fragment {
+public class SellerFragment extends Fragment implements SellerRecyclerAdapter.ItemClickListener{
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    private SellerRecyclerAdapter adapter;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -53,6 +63,29 @@ public class SellerFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("test a");
+        list.add("test b");
+        list.add("test c");
+
+        RecyclerView recyclerView = view.findViewById(R.id.sellerRecycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new SellerRecyclerAdapter(getContext(), list);
+        adapter.setClickListener(this);
+        Log.d("test", adapter.getItem(1));
+
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(getActivity(), "clicked on " + adapter.getItem(position), Toast.LENGTH_SHORT).show();
     }
 
     @Override
