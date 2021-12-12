dbaad064583528b20d0f2a22523654a2fdbb9d41
Rohan Ramachandran <rmramachandr@wisc.edu>
2021-12-12 09:49:38 -0600

    Fixed Seller and Transaction Helper Classes

--- app/src/main/java/com/example/feedmewithfirebase/UserHelperClass.java
+++ app/src/main/java/com/example/feedmewithfirebase/UserHelperClass.java
@@ -1,7 +1,7 @@
 package com.example.feedmewithfirebase;
 
 public class UserHelperClass {
-    String firstName, lastName, username, password, latitude, longitude;
+    public String firstName, lastName, username, password, latitude, longitude;
 
     public UserHelperClass(){}
 
@@ -14,51 +14,5 @@ public class UserHelperClass {
         this.longitude = longitude;
     }
 
-    public String getLastName() {
-        return lastName;
-    }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(String latitude) {
-        this.latitude = latitude;
-    }
-
-    public String getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(String longitude) {
-        this.longitude = longitude;
-    }
 }
